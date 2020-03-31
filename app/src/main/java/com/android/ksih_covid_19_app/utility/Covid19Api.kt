4c45815dc6a3b79ec6b335de4e22cc609d393218
Dablaze-ufc <chukwuwauchenna@gmail.com>
2020-03-31 23:55:03 +0100

    initial dev setup

---
+++ app/src/main/java/com/android/ksih_covid_19_app/utility/Covid19Api.kt
@@ -0,0 +1,5 @@
+package com.android.ksih_covid_19_app.utility
+
+interface Covid19Api {
+
+}
--- app/src/main/java/com/android/ksih_covid_19_app/utility/RetrofitBuilder.kt
+++ app/src/main/java/com/android/ksih_covid_19_app/utility/RetrofitBuilder.kt
@@ -1,10 +1,15 @@
 package com.android.ksih_covid_19_app.utility
 
 import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
 
 class RetrofitBuilder {
     val retrofit = Retrofit.Builder()
-        .baseUrl("https://api.covid19api.com/").build()
+        .baseUrl("https://api.covid19api.com/")
+        .addConverterFactory(GsonConverterFactory.create())
+        .build()
 
-
+        val covid19Api = retrofit.create(Covid19Api::class.java)
 }
+
+
