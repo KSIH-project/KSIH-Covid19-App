e4930fcc8b73d732f7b71a59b70f434ec49b63a3
Dablaze-ufc <chukwuwauchenna@gmail.com>
2020-04-02 22:34:05 +0100

    initial dev setup

--- app/src/main/java/com/android/ksih_covid_19_app/MainActivity.kt
+++ app/src/main/java/com/android/ksih_covid_19_app/MainActivity.kt
@@ -2,11 +2,16 @@ package com.android.ksih_covid_19_app
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var mNavController: NavController
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        mNavController = Navigation.findNavController(this, R.id.fragment)
     }
 }
