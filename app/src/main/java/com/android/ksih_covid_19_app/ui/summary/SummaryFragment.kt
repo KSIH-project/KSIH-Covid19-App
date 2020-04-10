c884de04b8977d2588ac67a7b398350b1917d511
sundaydavid <sundaydavid989@gmail.com>
2020-04-10 20:25:06 +0100

    add back button Navigation

--- app/src/main/java/com/android/ksih_covid_19_app/ui/summary/SummaryFragment.kt
+++ app/src/main/java/com/android/ksih_covid_19_app/ui/summary/SummaryFragment.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.LifecycleOwner
@@ -11,6 +12,7 @@ import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.ViewModelProviders
+import androidx.navigation.Navigation
 import androidx.recyclerview.widget.DividerItemDecoration
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
@@ -34,6 +36,8 @@ class SummaryFragment : Fragment() {
         }
 
         recyclerView = root.findViewById(R.id.summary_recyclerView)
+        val back = root.findViewById<ImageView>(R.id.go_back)
+        back.setOnClickListener { Navigation.findNavController(root).navigate(R.id.homeFragment) }
 
         return root
     }
