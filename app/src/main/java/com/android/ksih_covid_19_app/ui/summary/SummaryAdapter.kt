781159736a5e17da8ccf64b026235297034cad3e
sundaydavid <sundaydavid989@gmail.com>
2020-04-10 10:00:26 +0100

    Adding summary adapter class

---
+++ app/src/main/java/com/android/ksih_covid_19_app/ui/summary/SummaryAdapter.kt
@@ -0,0 +1,28 @@
+package com.android.ksih_covid_19_app.ui.summary
+
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.android.ksih_covid_19_app.model.Summary
+
+class SummaryAdapter(private val summary: ArrayList<Summary>) : RecyclerView.Adapter<SummaryAdapter.ViewHolder>(){
+
+
+
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        TODO("Not yet implemented")
+    }
+
+    override fun getItemCount(): Int {
+        TODO("Not yet implemented")
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        TODO("Not yet implemented")
+    }
+
+    inner class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView){
+
+    }
+}
