e4a380f1e3f6e6050ce8cba6bec631ad327654c1
sundaydavid <sundaydavid989@gmail.com>
2020-04-12 11:34:17 +0100

    complete home xml design

--- app/src/main/java/com/android/ksih_covid_19_app/MainActivity.kt
+++ app/src/main/java/com/android/ksih_covid_19_app/MainActivity.kt
@@ -10,8 +10,34 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.home_fragment)
+        setContentView(R.layout.activity_main)
 
-        //mNavController = Navigation.findNavController(this, R.id.fragment)
+        mNavController = Navigation.findNavController(this, R.id.fragment)
+        setDestinationListener()
+
+    }
+
+    private fun setDestinationListener(){
+        mNavController.addOnDestinationChangedListener{controller, destination, arguments ->
+            val dest = resources.getResourceName(destination.id)
+
+            when(destination.id) {
+               R.id.homeFragment, R.id.summaryFragment -> {
+                    hideCustomToolBar()
+                }
+                else -> {
+                    showCustomToolBar()
+                }
+            }
+        }
+
+    }
+
+    private fun hideCustomToolBar() {
+        supportActionBar?.hide()
+    }
+
+    private fun showCustomToolBar() {
+        supportActionBar?.show()
     }
 }
