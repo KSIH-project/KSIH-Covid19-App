e2b542f4c0bf48a1efe3b726b8304f7e1e57ccf5
SegunFrancis <francis4dblues@gmail.com>
2020-04-13 04:35:31 +0100

    KSIH Covid-19 - add temporary navigation

--- app/src/main/java/com/android/ksih_covid_19_app/ui/home/HomeFragment.kt
+++ app/src/main/java/com/android/ksih_covid_19_app/ui/home/HomeFragment.kt
@@ -5,8 +5,10 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.Navigation.findNavController
 
 import com.android.ksih_covid_19_app.R
+import kotlinx.android.synthetic.main.home_fragment.*
 
 /**
  * A simple [Fragment] subclass.
@@ -21,4 +23,11 @@ class HomeFragment : Fragment() {
         return inflater.inflate(R.layout.home_fragment, container, false)
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        // Todo: Remove temporary navigation
+        home_text.setOnClickListener {
+            findNavController(it).navigate(R.id.liveByCountryAndStatusFragment)
+        }
+    }
 }
