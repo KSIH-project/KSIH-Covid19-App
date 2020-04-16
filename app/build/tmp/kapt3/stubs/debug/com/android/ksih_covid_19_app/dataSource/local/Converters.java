38bf2b09763b5b24b59a77f8e7d0913d34747f73
Dablaze-ufc <chukwuwauchenna@gmail.com>
2020-04-16 23:40:16 +0100

    resolving conflicting gradle files

---
+++ app/build/tmp/kapt3/stubs/debug/com/android/ksih_covid_19_app/dataSource/local/Converters.java
@@ -0,0 +1,14 @@
+package com.android.ksih_covid_19_app.dataSource.local;
+
+import java.lang.System;
+
+/**
+ * Created by SegunFrancis
+ */
+@kotlin.Metadata(mv = {1, 1, 16}, bv = {1, 0, 3}, k = 1, d1 = {"\u0000\f\n\u0002\u0018\u0002\n\u0002\u0010\u0000\n\u0002\b\u0002\u0018\u00002\u00020\u0001B\u0005\u00a2\u0006\u0002\u0010\u0002\u00a8\u0006\u0003"}, d2 = {"Lcom/android/ksih_covid_19_app/dataSource/local/Converters;", "", "()V", "app_debug"})
+public final class Converters {
+    
+    public Converters() {
+        super();
+    }
+}
---
+++ app/build/tmp/kapt3/stubs/debug/com/android/ksih_covid_19_app/utility/Constants.java
@@ -0,0 +1,24 @@
+package com.android.ksih_covid_19_app.utility;
+
+import java.lang.System;
+
+@kotlin.Metadata(mv = {1, 1, 16}, bv = {1, 0, 3}, k = 1, d1 = {"\u0000\u001c\n\u0002\u0018\u0002\n\u0002\u0010\u0000\n\u0002\b\u0002\n\u0002\u0010\u000e\n\u0002\b\u0002\n\u0002\u0010\b\n\u0002\b\u0003\b\u00c6\u0002\u0018\u00002\u00020\u0001B\u0007\b\u0002\u00a2\u0006\u0002\u0010\u0002J\u0018\u0010\u0005\u001a\u00020\u00042\u0006\u0010\u0006\u001a\u00020\u00072\u0006\u0010\b\u001a\u00020\u0007H\u0002J\n\u0010\t\u001a\u00020\u0004*\u00020\u0004R\u000e\u0010\u0003\u001a\u00020\u0004X\u0086T\u00a2\u0006\u0002\n\u0000\u00a8\u0006\n"}, d2 = {"Lcom/android/ksih_covid_19_app/utility/Constants;", "", "()V", "BASE_URL", "", "percentageCase", "value", "", "confirmed", "toFlagEmoji", "app_debug"})
+public final class Constants {
+    @org.jetbrains.annotations.NotNull()
+    public static final java.lang.String BASE_URL = "https://api.covid19api.com/";
+    public static final com.android.ksih_covid_19_app.utility.Constants INSTANCE = null;
+    
+    @org.jetbrains.annotations.NotNull()
+    public final java.lang.String toFlagEmoji(@org.jetbrains.annotations.NotNull()
+    java.lang.String $this$toFlagEmoji) {
+        return null;
+    }
+    
+    private final java.lang.String percentageCase(int value, int confirmed) {
+        return null;
+    }
+    
+    private Constants() {
+        super();
+    }
+}
