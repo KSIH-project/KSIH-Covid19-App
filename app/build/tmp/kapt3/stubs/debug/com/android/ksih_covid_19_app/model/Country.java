79075348cf4e88b7b6270c544b85ec2344826c5d
peculiaruc <peculiarumeh02@gmail,com>
2020-04-17 02:09:20 +0100

    TASK FEATURE/PREVENTION:prevention bottom sheet

---
+++ app/build/tmp/kapt3/stubs/debug/com/android/ksih_covid_19_app/model/Country.java
@@ -0,0 +1,145 @@
+package com.android.ksih_covid_19_app.model;
+
+import java.lang.System;
+
+@androidx.room.Entity()
+@kotlin.Metadata(mv = {1, 1, 16}, bv = {1, 0, 3}, k = 1, d1 = {"\u0000(\n\u0002\u0018\u0002\n\u0002\u0018\u0002\n\u0000\n\u0002\u0010\u000e\n\u0002\b\u0003\n\u0002\u0010\b\n\u0002\b\u001f\n\u0002\u0010\u000b\n\u0000\n\u0002\u0010\u0000\n\u0002\b\u0003\b\u0087\b\u0018\u00002\u00020\u0001BU\u0012\u0006\u0010\u0002\u001a\u00020\u0003\u0012\u0006\u0010\u0004\u001a\u00020\u0003\u0012\u0006\u0010\u0005\u001a\u00020\u0003\u0012\u0006\u0010\u0006\u001a\u00020\u0007\u0012\u0006\u0010\b\u001a\u00020\u0007\u0012\u0006\u0010\t\u001a\u00020\u0007\u0012\u0006\u0010\n\u001a\u00020\u0003\u0012\u0006\u0010\u000b\u001a\u00020\u0007\u0012\u0006\u0010\f\u001a\u00020\u0007\u0012\u0006\u0010\r\u001a\u00020\u0007\u00a2\u0006\u0002\u0010\u000eJ\t\u0010\u001b\u001a\u00020\u0003H\u00c6\u0003J\t\u0010\u001c\u001a\u00020\u0007H\u00c6\u0003J\t\u0010\u001d\u001a\u00020\u0003H\u00c6\u0003J\t\u0010\u001e\u001a\u00020\u0003H\u00c6\u0003J\t\u0010\u001f\u001a\u00020\u0007H\u00c6\u0003J\t\u0010 \u001a\u00020\u0007H\u00c6\u0003J\t\u0010!\u001a\u00020\u0007H\u00c6\u0003J\t\u0010\"\u001a\u00020\u0003H\u00c6\u0003J\t\u0010#\u001a\u00020\u0007H\u00c6\u0003J\t\u0010$\u001a\u00020\u0007H\u00c6\u0003Jm\u0010%\u001a\u00020\u00002\b\b\u0002\u0010\u0002\u001a\u00020\u00032\b\b\u0002\u0010\u0004\u001a\u00020\u00032\b\b\u0002\u0010\u0005\u001a\u00020\u00032\b\b\u0002\u0010\u0006\u001a\u00020\u00072\b\b\u0002\u0010\b\u001a\u00020\u00072\b\b\u0002\u0010\t\u001a\u00020\u00072\b\b\u0002\u0010\n\u001a\u00020\u00032\b\b\u0002\u0010\u000b\u001a\u00020\u00072\b\b\u0002\u0010\f\u001a\u00020\u00072\b\b\u0002\u0010\r\u001a\u00020\u0007H\u00c6\u0001J\u0013\u0010&\u001a\u00020\'2\b\u0010(\u001a\u0004\u0018\u00010)H\u00d6\u0003J\t\u0010*\u001a\u00020\u0007H\u00d6\u0001J\t\u0010+\u001a\u00020\u0003H\u00d6\u0001R\u0016\u0010\u0002\u001a\u00020\u00038\u0006X\u0087\u0004\u00a2\u0006\b\n\u0000\u001a\u0004\b\u000f\u0010\u0010R\u0011\u0010\u0004\u001a\u00020\u0003\u00a2\u0006\b\n\u0000\u001a\u0004\b\u0011\u0010\u0010R\u0011\u0010\u0005\u001a\u00020\u0003\u00a2\u0006\b\n\u0000\u001a\u0004\b\u0012\u0010\u0010R\u0011\u0010\u0006\u001a\u00020\u0007\u00a2\u0006\b\n\u0000\u001a\u0004\b\u0013\u0010\u0014R\u0011\u0010\b\u001a\u00020\u0007\u00a2\u0006\b\n\u0000\u001a\u0004\b\u0015\u0010\u0014R\u0011\u0010\t\u001a\u00020\u0007\u00a2\u0006\b\n\u0000\u001a\u0004\b\u0016\u0010\u0014R\u0011\u0010\n\u001a\u00020\u0003\u00a2\u0006\b\n\u0000\u001a\u0004\b\u0017\u0010\u0010R\u0011\u0010\u000b\u001a\u00020\u0007\u00a2\u0006\b\n\u0000\u001a\u0004\b\u0018\u0010\u0014R\u0011\u0010\f\u001a\u00020\u0007\u00a2\u0006\b\n\u0000\u001a\u0004\b\u0019\u0010\u0014R\u0011\u0010\r\u001a\u00020\u0007\u00a2\u0006\b\n\u0000\u001a\u0004\b\u001a\u0010\u0014\u00a8\u0006,"}, d2 = {"Lcom/android/ksih_covid_19_app/model/Country;", "Ljava/io/Serializable;", "Country", "", "CountryCode", "Date", "NewConfirmed", "", "NewDeaths", "NewRecovered", "Slug", "TotalConfirmed", "TotalDeaths", "TotalRecovered", "(Ljava/lang/String;Ljava/lang/String;Ljava/lang/String;IIILjava/lang/String;III)V", "getCountry", "()Ljava/lang/String;", "getCountryCode", "getDate", "getNewConfirmed", "()I", "getNewDeaths", "getNewRecovered", "getSlug", "getTotalConfirmed", "getTotalDeaths", "getTotalRecovered", "component1", "component10", "component2", "component3", "component4", "component5", "component6", "component7", "component8", "component9", "copy", "equals", "", "other", "", "hashCode", "toString", "app_debug"})
+public final class Country implements java.io.Serializable {
+    @org.jetbrains.annotations.NotNull()
+    @androidx.room.PrimaryKey()
+    private final java.lang.String Country = null;
+    @org.jetbrains.annotations.NotNull()
+    private final java.lang.String CountryCode = null;
+    @org.jetbrains.annotations.NotNull()
+    private final java.lang.String Date = null;
+    private final int NewConfirmed = 0;
+    private final int NewDeaths = 0;
+    private final int NewRecovered = 0;
+    @org.jetbrains.annotations.NotNull()
+    private final java.lang.String Slug = null;
+    private final int TotalConfirmed = 0;
+    private final int TotalDeaths = 0;
+    private final int TotalRecovered = 0;
+    
+    @org.jetbrains.annotations.NotNull()
+    public final java.lang.String getCountry() {
+        return null;
+    }
+    
+    @org.jetbrains.annotations.NotNull()
+    public final java.lang.String getCountryCode() {
+        return null;
+    }
+    
+    @org.jetbrains.annotations.NotNull()
+    public final java.lang.String getDate() {
+        return null;
+    }
+    
+    public final int getNewConfirmed() {
+        return 0;
+    }
+    
+    public final int getNewDeaths() {
+        return 0;
+    }
+    
+    public final int getNewRecovered() {
+        return 0;
+    }
+    
+    @org.jetbrains.annotations.NotNull()
+    public final java.lang.String getSlug() {
+        return null;
+    }
+    
+    public final int getTotalConfirmed() {
+        return 0;
+    }
+    
+    public final int getTotalDeaths() {
+        return 0;
+    }
+    
+    public final int getTotalRecovered() {
+        return 0;
+    }
+    
+    public Country(@org.jetbrains.annotations.NotNull()
+    java.lang.String Country, @org.jetbrains.annotations.NotNull()
+    java.lang.String CountryCode, @org.jetbrains.annotations.NotNull()
+    java.lang.String Date, int NewConfirmed, int NewDeaths, int NewRecovered, @org.jetbrains.annotations.NotNull()
+    java.lang.String Slug, int TotalConfirmed, int TotalDeaths, int TotalRecovered) {
+        super();
+    }
+    
+    @org.jetbrains.annotations.NotNull()
+    public final java.lang.String component1() {
+        return null;
+    }
+    
+    @org.jetbrains.annotations.NotNull()
+    public final java.lang.String component2() {
+        return null;
+    }
+    
+    @org.jetbrains.annotations.NotNull()
+    public final java.lang.String component3() {
+        return null;
+    }
+    
+    public final int component4() {
+        return 0;
+    }
+    
+    public final int component5() {
+        return 0;
+    }
+    
+    public final int component6() {
+        return 0;
+    }
+    
+    @org.jetbrains.annotations.NotNull()
+    public final java.lang.String component7() {
+        return null;
+    }
+    
+    public final int component8() {
+        return 0;
+    }
+    
+    public final int component9() {
+        return 0;
+    }
+    
+    public final int component10() {
+        return 0;
+    }
+    
+    @org.jetbrains.annotations.NotNull()
+    public final com.android.ksih_covid_19_app.model.Country copy(@org.jetbrains.annotations.NotNull()
+    java.lang.String Country, @org.jetbrains.annotations.NotNull()
+    java.lang.String CountryCode, @org.jetbrains.annotations.NotNull()
+    java.lang.String Date, int NewConfirmed, int NewDeaths, int NewRecovered, @org.jetbrains.annotations.NotNull()
+    java.lang.String Slug, int TotalConfirmed, int TotalDeaths, int TotalRecovered) {
+        return null;
+    }
+    
+    @org.jetbrains.annotations.NotNull()
+    @java.lang.Override()
+    public java.lang.String toString() {
+        return null;
+    }
+    
+    @java.lang.Override()
+    public int hashCode() {
+        return 0;
+    }
+    
+    @java.lang.Override()
+    public boolean equals(@org.jetbrains.annotations.Nullable()
+    java.lang.Object p0) {
+        return false;
+    }
+}
