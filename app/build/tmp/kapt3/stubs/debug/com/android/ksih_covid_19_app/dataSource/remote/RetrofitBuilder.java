8c50a3c8ddacd0780ba95775a796daa78100600a
dablaze-ufc <chukwuwauchenna@gmail.com>
2020-04-22 22:58:54 +0100

    fixed the issues with git

---
+++ app/build/tmp/kapt3/stubs/debug/com/android/ksih_covid_19_app/dataSource/remote/RetrofitBuilder.java
@@ -0,0 +1,20 @@
+package com.android.ksih_covid_19_app.dataSource.remote;
+
+import java.lang.System;
+
+@kotlin.Metadata(mv = {1, 1, 16}, bv = {1, 0, 3}, k = 1, d1 = {"\u0000\u001a\n\u0002\u0018\u0002\n\u0002\u0010\u0000\n\u0002\b\u0002\n\u0002\u0018\u0002\n\u0002\b\u0003\n\u0002\u0018\u0002\n\u0000\b\u00c6\u0002\u0018\u00002\u00020\u0001B\u0007\b\u0002\u00a2\u0006\u0002\u0010\u0002R\u0011\u0010\u0003\u001a\u00020\u0004\u00a2\u0006\b\n\u0000\u001a\u0004\b\u0005\u0010\u0006R\u000e\u0010\u0007\u001a\u00020\bX\u0082\u0004\u00a2\u0006\u0002\n\u0000\u00a8\u0006\t"}, d2 = {"Lcom/android/ksih_covid_19_app/dataSource/remote/RetrofitBuilder;", "", "()V", "covid19Api", "Lcom/android/ksih_covid_19_app/dataSource/remote/Covid19Api;", "getCovid19Api", "()Lcom/android/ksih_covid_19_app/dataSource/remote/Covid19Api;", "retrofit", "Lretrofit2/Retrofit;", "app_debug"})
+public final class RetrofitBuilder {
+    private static final retrofit2.Retrofit retrofit = null;
+    @org.jetbrains.annotations.NotNull()
+    private static final com.android.ksih_covid_19_app.dataSource.remote.Covid19Api covid19Api = null;
+    public static final com.android.ksih_covid_19_app.dataSource.remote.RetrofitBuilder INSTANCE = null;
+    
+    @org.jetbrains.annotations.NotNull()
+    public final com.android.ksih_covid_19_app.dataSource.remote.Covid19Api getCovid19Api() {
+        return null;
+    }
+    
+    private RetrofitBuilder() {
+        super();
+    }
+}
---
+++ app/build/tmp/kapt3/stubs/debug/com/android/ksih_covid_19_app/ui/summary/SummaryViewModel.java
@@ -0,0 +1,32 @@
+package com.android.ksih_covid_19_app.ui.summary;
+
+import java.lang.System;
+
+@kotlin.Metadata(mv = {1, 1, 16}, bv = {1, 0, 3}, k = 1, d1 = {"\u0000 \n\u0002\u0018\u0002\n\u0002\u0018\u0002\n\u0002\b\u0002\n\u0002\u0018\u0002\n\u0002\u0010\u000b\n\u0002\b\u0005\n\u0002\u0010\u000e\n\u0002\b\u0002\b\u0016\u0018\u00002\u00020\u0001B\u0005\u00a2\u0006\u0002\u0010\u0002R\u0017\u0010\u0003\u001a\b\u0012\u0004\u0012\u00020\u00050\u0004\u00a2\u0006\b\n\u0000\u001a\u0004\b\u0006\u0010\u0007R\u0017\u0010\b\u001a\b\u0012\u0004\u0012\u00020\u00050\u0004\u00a2\u0006\b\n\u0000\u001a\u0004\b\t\u0010\u0007R\u0017\u0010\n\u001a\b\u0012\u0004\u0012\u00020\u000b0\u0004\u00a2\u0006\b\n\u0000\u001a\u0004\b\f\u0010\u0007\u00a8\u0006\r"}, d2 = {"Lcom/android/ksih_covid_19_app/ui/summary/SummaryViewModel;", "Landroidx/lifecycle/ViewModel;", "()V", "dataLoading", "Landroidx/lifecycle/MutableLiveData;", "", "getDataLoading", "()Landroidx/lifecycle/MutableLiveData;", "empty", "getEmpty", "toastMessage", "", "getToastMessage", "app_debug"})
+public class SummaryViewModel extends androidx.lifecycle.ViewModel {
+    @org.jetbrains.annotations.NotNull()
+    private final androidx.lifecycle.MutableLiveData<java.lang.Boolean> empty = null;
+    @org.jetbrains.annotations.NotNull()
+    private final androidx.lifecycle.MutableLiveData<java.lang.Boolean> dataLoading = null;
+    @org.jetbrains.annotations.NotNull()
+    private final androidx.lifecycle.MutableLiveData<java.lang.String> toastMessage = null;
+    
+    @org.jetbrains.annotations.NotNull()
+    public final androidx.lifecycle.MutableLiveData<java.lang.Boolean> getEmpty() {
+        return null;
+    }
+    
+    @org.jetbrains.annotations.NotNull()
+    public final androidx.lifecycle.MutableLiveData<java.lang.Boolean> getDataLoading() {
+        return null;
+    }
+    
+    @org.jetbrains.annotations.NotNull()
+    public final androidx.lifecycle.MutableLiveData<java.lang.String> getToastMessage() {
+        return null;
+    }
+    
+    public SummaryViewModel() {
+        super();
+    }
+}
