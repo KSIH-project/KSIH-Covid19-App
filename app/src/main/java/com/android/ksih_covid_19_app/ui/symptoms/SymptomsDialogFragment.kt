257edf925861c2cb7203b63446a842ed3e2398ba
peculiaruc <peculiarumeh02@gmail,com>
2020-04-24 06:39:04 +0100

    TASK FEATURE/PREVENTION:prevention bottom sheet

--- app/src/main/java/com/android/ksih_covid_19_app/ui/symptoms/SymptomsDialogFragment.kt
+++ app/src/main/java/com/android/ksih_covid_19_app/ui/symptoms/SymptomsDialogFragment.kt
@@ -1,22 +1,23 @@
 package com.android.ksih_covid_19_app.ui.symptoms
 
 import android.app.AlertDialog
-import android.app.Dialog
 import android.content.DialogInterface
 import android.os.Bundle
 import android.util.Log
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
 import androidx.fragment.app.DialogFragment
-
+import androidx.fragment.app.Fragment
 import com.android.ksih_covid_19_app.R
+import org.jetbrains.anko.support.v4.progressDialog
+
 
 /**
  * A simple [Fragment] subclass.
  */
+@Suppress("UNREACHABLE_CODE")
 class SymptomsDialogFragment : DialogFragment() {
 
     private var content: String? = null
@@ -29,25 +30,28 @@ class SymptomsDialogFragment : DialogFragment() {
         setStyle(style, theme)
     }
 
-
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
 
         val root = inflater.inflate(R.layout.fragment_symptoms_dialog, container, false)
-
         val symptoms = arguments?.getSerializable("symptoms") as SymptomsData
         val title = root.findViewById<TextView>(R.id.texttitle)
         val textsymptoms = root.findViewById<TextView>(R.id.textsymptoms)
         title.text = symptoms.title
         textsymptoms.text = symptoms.symptoms
-
         Log.d("dialogFragment", "DialogText ${symptoms}")
+
+
+        val builder: AlertDialog.Builder = AlertDialog.Builder(activity)
+        builder.setIcon(R.drawable.symptoms)
+            .setTitle(R.string.title_dialog)
+            .setMessage(R.string.symptom_dialog)
+
         return root
-
-
     }
 
-
 }
+
+
