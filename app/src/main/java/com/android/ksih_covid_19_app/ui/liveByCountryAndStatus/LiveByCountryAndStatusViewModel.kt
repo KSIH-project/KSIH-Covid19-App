90a9327b4b76b9651e9c69b6debe98975a4393a6
Dablaze-ufc <chukwuwauchenna@gmail.com>
2020-05-12 16:36:11 +0100

    renamed to project and fixed a few bugs

--- app/src/main/java/com/android/ksih_covid_19_app/ui/liveByCountryAndStatus/LiveByCountryAndStatusViewModel.kt
+++ app/src/main/java/com/android/ksih_covid_19_app/ui/liveByCountryAndStatus/LiveByCountryAndStatusViewModel.kt
@@ -52,7 +52,7 @@ class LiveByCountryAndStatusViewModel(myApplication: Application) :
         repository.getSummary().enqueue(object : Callback<Summary?> {
             override fun onResponse(call: Call<Summary?>, response: Response<Summary?>) {
                 viewModelScope.launch {
-                    repository.setCountryAndNewCasesListLocal(response.body()!!.Countries)
+                    response.body()?.Countries?.let { repository.setCountryAndNewCasesListLocal(it) }
                 }
                 responseMessage.postValue(
                     Event(
